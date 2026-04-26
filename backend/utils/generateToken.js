77099ac5bc6cb754b3b2366e871eb4786b2ec079
Sanam Rai <sanamr571@outlook.com>
2026-04-26 16:45:24 +0545

    phase 2 complete

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,6 +4,6 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 export const generateToken = (userId) =>{
-    return token = jwt.sign({id: userId}, process.env.JWT_SECRET_KEY, {expiresIn: '1h'});
+    return jwt.sign({id: userId}, process.env.JWT_SECRET_KEY, {expiresIn: '1h'});
 }
 
