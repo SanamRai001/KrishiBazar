aa295022a9a9863c7dc09dba67b91baf227b74fc
Sanam Rai <sanamr571@outlook.com>
2026-04-26 16:40:47 +0545

    added authmiddleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const authMiddleware = (req, res, next) =>{
+    const token = req.headers['authorization']?.split(' ')[1];
+    if(!token){
+        return res.status(401).json({
+            success: false,
+            message: "Access Denied"
+        });
+    }
+    try{
+        req.user = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        next();
+    }
+    catch(err){
+        console.error("Error while verifying  Token!");
+        return res.status(403).json({
+            success:false,
+            message: "Invalid  Token"
+        });
+    }
+};
+
+export default  authMiddleware;
