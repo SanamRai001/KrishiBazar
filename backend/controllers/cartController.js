360a5e9057eb92c790500de2052c71cd6c955dc3
Sanam Rai <sanamr571@outlook.com>
2026-04-29 20:12:45 +0545

    cart crud completed

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -66,5 +66,81 @@ export const addCart = async (req, res) => {
         });
     }
 };
-export const updateCart = async (req, res) => {};
-export const deleteCart = async (req, res) => {};
+export const updateCart = async (req, res) => {
+    const productId = req.params.id;
+    const user = req.user.id;
+    const {quantity} = req.body;
+    try{
+        const userCart = await Cart.findOne({user: user});
+        if(userCart){
+            const search = userCart.items.find((i)=>{
+                return productId.toString() == i.product.toString();
+            });
+            if(!search){
+                return  res.json({
+                    success: true,
+                    message: "No such Product is found to update in cart.",
+                    data: userCart
+                })
+            }
+            if(quantity === 0){
+                const newItems = userCart.items.filter((i)=>i.product.toString() !== productId.toString());
+                userCart.items = newItems;
+                await userCart.save();
+                return res.json({
+                    success: true,
+                    message: "Item removed  from cart"
+                });
+            }
+            search.quantity = quantity;
+            await userCart.save();
+            res.json({
+                success: true,
+                message: "Cart updated successfully",
+                data: userCart
+            });
+        }
+        else{
+            return res.json({
+                success: true,
+                message: "No cart found"
+            });
+        }
+    }
+    catch(err){
+        console.error("Error updating Cart: ", err);
+        res.json({
+            success: false,
+            message: "Error  while updating Cart"
+        });
+    }
+};
+export const deleteCart = async (req, res) => {
+    const productId = req.params.id;
+    const user = req.user.id;
+    try{
+        const userCart = await Cart.findOne({user: user});
+        if(userCart){
+           const newItems = userCart.items.filter((i)=>i.product.toString() !== productId.toString());
+            userCart.items = newItems;
+            await userCart.save();
+            return res.json({
+                success: true,
+                message: "Item removed  from cart"
+            });
+        }
+        else{
+            return res.json({
+                success: true,
+                message: "Cart not found"
+            })
+        }
+    }
+    catch(err){
+        console.error("Error  while  removing  items from cart");
+        res.json({
+            success: false,
+            message: "Error  while removing items from Cart"
+        });
+    }
+};
