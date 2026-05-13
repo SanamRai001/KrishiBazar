f192bdce3e665d6abde0f9e98e093d71e3f051e8
Sanam Rai <sanamr571@outlook.com>
2026-05-13 21:27:20 +0545

    added cartContext with fetchCart and removefromCart

--- frontend/KrishiBazar/src/context/CartContext.jsx
+++ frontend/KrishiBazar/src/context/CartContext.jsx
@@ -1,23 +1,55 @@
 import { createContext, useState } from "react";
+import axiosInstance from "../axiosInstance";
+
 
 export const CartContext =  createContext();
 export const CartProvider = ({children}) =>{
     const [cart, setCart] = useState([]);
     const itemCount = cart.length;
-    const addToCart = (cart, item, quantity) =>{
-       
+    const fetchCart = async () =>{
+        try{
+            const response = await axiosInstance.get("/cart");
+            if(response.data.success){
+                setCart(response.data.data);
+            }
+            else{
+                console.log(response.data.message);
+            }
+        }
+        catch(err){
+            console.error("Error while fetching Cart: ", err.message);
+        }
     }
-    const removeFromCart = () =>{
+    const addToCart = async (product, quantity) =>{
+        try{
 
+        }
+        catch(err){
+
+        }
     }
-    const updateQuantity = () =>{
+    const removeFromCart = async (product) =>{
+        try{
+            const response = await axiosInstance.delete(`/cart/:${product}`);
+            if(response.data.success){
+                fetchCart();
+            }
+            else{
+                console.log(response.data.message);
+            }
+        }
+        catch(err){
+            console.error("Error while removing  an  item",  err.message);
+        }
+    }
+    const updateQuantity = async () =>{
 
     }
     const clearCart = () =>{
 
     }
     return (
-        <CartContext.Provider value={{cart, itemCount, addToCart, removeFromCart, updateQuantity,  clearCart}}>
+        <CartContext.Provider value={{cart, itemCount, fetchCart, addToCart, removeFromCart, updateQuantity,  clearCart}}>
             {children}
         </CartContext.Provider>
     )
