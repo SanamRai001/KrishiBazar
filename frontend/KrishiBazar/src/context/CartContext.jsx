16889af602a60db270230af46589f8062ae86b50
Sanam Rai <sanamr571@outlook.com>
2026-05-14 18:28:28 +0545

    added full logic  of CartContext and added  provider in main.jsx

--- frontend/KrishiBazar/src/context/CartContext.jsx
+++ frontend/KrishiBazar/src/context/CartContext.jsx
@@ -6,6 +6,8 @@ export const CartContext =  createContext();
 export const CartProvider = ({children}) =>{
     const [cart, setCart] = useState([]);
     const itemCount = cart.length;
+
+
     const fetchCart = async () =>{
         try{
             const response = await axiosInstance.get("/cart");
@@ -22,15 +24,21 @@ export const CartProvider = ({children}) =>{
     }
     const addToCart = async (product, quantity) =>{
         try{
-
+            const response = await axiosInstance.post("/cart", {product, quantity});
+            if(response.data.success){
+                fetchCart();
+            }
+            else{
+                console.log(response.data.message);
+            }
         }
         catch(err){
-
+            console.error("Error while adding Product to Cart: ", err.message);
         }
     }
     const removeFromCart = async (product) =>{
         try{
-            const response = await axiosInstance.delete(`/cart/:${product}`);
+            const response = await axiosInstance.delete(`/cart/${product}`);
             if(response.data.success){
                 fetchCart();
             }
@@ -42,11 +50,22 @@ export const CartProvider = ({children}) =>{
             console.error("Error while removing  an  item",  err.message);
         }
     }
-    const updateQuantity = async () =>{
-
+    const updateQuantity = async (product, quantity) =>{
+        try{
+            const response = await axiosInstance.put(`/cart/${product}`, {quantity});
+            if(response.data.success){
+                fetchCart();
+            }
+            else{
+                console.log(response.data.message);
+            }
+        }
+        catch(err){
+            console.error("Error while Updating Product to Cart: ", err.message);
+        }
     }
     const clearCart = () =>{
-
+        setCart([]);
     }
     return (
         <CartContext.Provider value={{cart, itemCount, fetchCart, addToCart, removeFromCart, updateQuantity,  clearCart}}>
