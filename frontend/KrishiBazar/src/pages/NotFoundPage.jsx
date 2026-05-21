6779660f0f890d44854412f544fc86e074d57935
Sanam Rai <sanamr571@outlook.com>
2026-05-21 21:23:09 +0545

    added notfound page

--- frontend/KrishiBazar/src/pages/NotFoundPage.jsx
+++ frontend/KrishiBazar/src/pages/NotFoundPage.jsx
@@ -1,7 +1,19 @@
-
+import { Link } from "react-router-dom"
 const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
+    <>
+      <div className="notFoundPage">
+        <img src="/notFound.gif" alt="Page Not found Gif" />
+        <p>404 Not found</p>
+        <h1>
+          Oops! Page Not Found
+        </h1>
+        <p>The page you are looking for does not exist. Click button below to go to the Homepage</p>
+        <Link to="/">
+        <button type="button">Back to Homepage</button>
+        </Link>
+      </div>    
+    </>
   )
 }
 
