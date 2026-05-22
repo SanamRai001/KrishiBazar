323349a41977bc099b0fbb80f8367281f4013c35
Sanam Rai <sanamr571@outlook.com>
2026-05-22 07:58:22 +0545

    styled page not found page and added png

--- frontend/KrishiBazar/src/pages/NotFoundPage.jsx
+++ frontend/KrishiBazar/src/pages/NotFoundPage.jsx
@@ -3,14 +3,14 @@ const NotFoundPage = () => {
   return (
     <>
       <div className="notFoundPage">
-        <img src="/notFound.gif" alt="Page Not found Gif" />
+        <img src="/notFound.png" alt="Page Not found Gif" height="400px" width="400px"/>
         <p>404 Not found</p>
         <h1>
           Oops! Page Not Found
         </h1>
         <p>The page you are looking for does not exist. Click button below to go to the Homepage</p>
-        <Link to="/">
-        <button type="button">Back to Homepage</button>
+        <Link to="/" className="backToHome">
+        Back to Homepage
         </Link>
       </div>    
     </>
