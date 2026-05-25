e6364f39c91d11692c3e08d8b37bd62554b8afb8
Sanam Rai <sanamr571@outlook.com>
2026-05-25 08:07:47 +0545

    removed unnecessary console

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,7 +1,6 @@
 import jwt from 'jsonwebtoken'
 
 const authMiddleware = (req, res, next) =>{
-    console.log("VERIFY SECRET:", process.env.JWT_SECRET_KEY);
     const token = req.headers['authorization']?.split(' ')[1];
     if(!token){
         return res.status(401).json({
