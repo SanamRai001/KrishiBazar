23dd0d88c2d408ccb1977581961eca7e79dd3880
Sanam Rai <sanamr571@outlook.com>
2026-05-25 08:05:15 +0545

    updated  cors to  vercel link

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ connectDB();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://krishi-bazar-alpha.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
